0fefd31c3a887ce082f5cb16552f9d46ee8a6031
Ubuntu <ubuntu@ip-172-31-16-226.us-east-2.compute.internal>
2021-08-31 05:04:04 +0000

    docs generated, description added, sources added, freshness check added

--- models/customers.sql
+++ models/customers.sql
@@ -8,7 +8,7 @@ with customer_orders as (
         count(*) as number_of_orders,
         min(created_at) as first_order_at,
         sum(total) as total_order_value
-    from `analytics-engineers-club.coffee_shop.orders`
+    from {{ source('coffee_shop', 'orders') }}
     group by 1
 )
 
@@ -20,5 +20,5 @@ select
     co.number_of_orders,
     co.total_order_value
 
-from `analytics-engineers-club.coffee_shop.customers` as c
+from {{ source('coffee_shop', 'customers') }} as c
 left join customer_orders as co on c.id = co.customer_id
